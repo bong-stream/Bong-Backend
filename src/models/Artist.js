b249a1b4f2c6ef015dd326e724c9aeee7e8062bb
bong-stream <75524801+bong-stream@users.noreply.github.com>
2020-12-09 14:12:32 +0500

    Update Artist.js

--- src/models/Artist.js
+++ src/models/Artist.js
@@ -5,6 +5,11 @@ const ArtistSchema = new mongoose.Schema({
   artistname: { type: String, required: true },
   artistimage: { type: String, required: true },
   artistlikes: { type: Number },
+<<<<<<< Updated upstream
+=======
+
+  // __v: { type: Number, select: false },
+>>>>>>> Stashed changes
 });
 
 //ArtistSchema.plugin(mongoosastic);
