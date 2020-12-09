4ae8457f1c90cf81a523ac2d2f8683a5f3fffba3
bong-stream <75524801+bong-stream@users.noreply.github.com>
2020-12-09 13:46:47 +0500

    Changes MAde by tasadduq according to front end of admin

--- src/models/Artist.js
+++ src/models/Artist.js
@@ -1,10 +1,13 @@
 const mongoose = require("mongoose");
-const mongoosastic = require('mongoosastic');
+const mongoosastic = require("mongoosastic");
 
 const ArtistSchema = new mongoose.Schema({
+  albums: { type: Array, required: true },
   artistname: { type: String, required: true },
   artistimage: { type: String, required: true },
   artistlikes: { type: Number },
+
+  __v: { type: Number, select: false },
 });
 
 ArtistSchema.plugin(mongoosastic);
