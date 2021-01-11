808e46c89eb953022ccce8a0ff5287d1cb4af428
Rao Zain <raozain70@gmail.com>
2021-01-11 13:47:45 +0500

    Update authRoutes.js

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -1,54 +1,54 @@
-const express = require("express");
-const mongoose = require("mongoose");
-const jwt = require("jsonwebtoken");
-const User = mongoose.model("User");
-const SendOtp = require("sendotp");
-const nodemailer = require("nodemailer");
-const router = express.Router();
-var otpGenerator = require("otp-generator");
-const Emailotp = require("../models/Emailotp");
-var emailOtp;
+const express = require("express")
+const mongoose = require("mongoose")
+const jwt = require("jsonwebtoken")
+const User = mongoose.model("User")
+const SendOtp = require("sendotp")
+const nodemailer = require("nodemailer")
+const router = express.Router()
+var otpGenerator = require("otp-generator")
+const Emailotp = require("../models/Emailotp")
+var emailOtp
 
-const sendOtp = new SendOtp("9146A4XsuWgB5fe97a0fP123");
+const sendOtp = new SendOtp("9146A4XsuWgB5fe97a0fP123")
 
 router.post("/sendotp", (req, res) => {
   if (req.body.phoneNumber) {
-    console.log(sendOtp);
+    // console.log(sendOtp)
     sendOtp.send(req.body.phoneNumber, "id_of_send", "4635", (err, data) => {
-      console.log(data);
-      if (err) return res.json({ err });
+      console.log(data)
+      if (err) return res.json({ err })
       data.type == "success"
         ? res.json({ success: true, message: data.message })
-        : res.json({ success: false, message: data.message });
-    });
+        : res.json({ success: false, message: data.message })
+    })
   } else {
   }
-});
+})
 
 router.post("/verify", (req, res) => {
   sendOtp.verify(req.body.phoneNumber, req.body.otp, function (err, data) {
-    if (err) return res.json({ err });
+    if (err) return res.json({ err })
     if (data.type == "success") {
-      let { phoneNumber } = req.body;
+      let { phoneNumber } = req.body
       User.findOne({ phoneNumber }, (err, user) => {
-        if (err) return res.json({ err });
+        if (err) return res.json({ err })
         if (!user) {
           // user signup
-          console.log("new user");
-          res.json({ success: true, message: data.message });
+          console.log("new user")
+          res.json({ success: true, message: data.message })
         }
         if (user) {
           // user signin
-          console.log("old user");
-          res.json({ success: false, message: data.message });
+          console.log("old user")
+          res.json({ success: false, message: data.message })
         }
-      });
+      })
     }
     if (data.type == "error") {
-      res.json({ success: false, message: data.message });
+      res.json({ success: false, message: data.message })
     }
-  });
-});
+  })
+})
 
 const sendEmailOtp = (otp, email) => {
   if (otp && email) {
@@ -60,7 +60,7 @@ const sendEmailOtp = (otp, email) => {
             </ul>
             <h3>OTP</h3>
             <p>${otp}</p>
-            `;
+            `
 
     // create reusable transporter object using the default SMTP transport
     let transporter = nodemailer.createTransport({
@@ -73,7 +73,7 @@ const sendEmailOtp = (otp, email) => {
         user: "queryaidataron@gmail.com", // generated ethereal user
         pass: "nwnxovucjfoqqwww", // generated ethereal password
       },
-    });
+    })
 
     // setup email data with unicode symbols
     let mailOptions = {
@@ -82,87 +82,89 @@ const sendEmailOtp = (otp, email) => {
       // subject: subject, // Subject line
       // text: details, // plain text body
       html: output, // html body
-    };
+    }
 
     // send mail with defined transport object
     transporter.sendMail(mailOptions, async (error, info) => {
       if (error) {
-        return error;
-        console.log("there is error");
+        return error
+        console.log("there is error")
       } else {
-        console.log("Message sent: %s", info.messageId);
-        console.log("Preview URL: %s", nodemailer.getTestMessageUrl(info));
+        console.log("Message sent: %s", info.messageId)
+        console.log("Preview URL: %s", nodemailer.getTestMessageUrl(info))
 
         let emailOtp = new Emailotp({
           email,
           otp,
-        });
-        await emailOtp.save();
+        })
+        await emailOtp.save()
         // res.status(200).json({ message: "Check Your Email" });
         // return true;
       }
-    });
-    return true;
+    })
+    return true
   } else {
     // res.status(401).json({ message: "Something went Wrong" });
-    return false;
+    return false
   }
-};
+}
 
 router.post("/sendemailotp", (req, res) => {
-  let { email } = req.body;
+  let { email } = req.body
   let otp = otpGenerator.generate(4, {
     upperCase: false,
     specialChars: false,
     alphabets: false,
-  });
+  })
 
   if (email && otp) {
     Emailotp.findOne({ email }).then(async (user) => {
       if (user) {
-        res.status(401).json({ message: "User Email already used" });
+        res
+          .status(401)
+          .json({ success: false, message: "User Email already used" })
       } else {
-        let response = sendEmailOtp(otp, email);
-        console.log(response);
+        let response = sendEmailOtp(otp, email)
+        console.log(response)
         if (response) {
-          res.status(200).json({ success: true });
+          res.status(200).json({ success: true })
         } else {
           res
             .status(401)
-            .json({ success: false, message: "Something went wrong" });
+            .json({ success: false, message: "Something went wrong" })
         }
       }
-    });
+    })
   } else {
-    res.status(401).json({ message: "Somthing Went Wrong" });
+    res.status(401).json({ success: false, message: "Somthing Went Wrong" })
   }
-});
+})
 
 router.post("/verifyemailotp", (req, res) => {
-  let { otp, email } = req.body;
+  let { otp, email } = req.body
   Emailotp.findOne({ email }).then(async (user) => {
     if (user) {
       user.otp === otp
-        ? res.status(200).json({ message: "User Verified" })
-        : res.status(401).json({ message: "Wrong OTP" });
+        ? res.status(200).json({ success: true, message: "User Verified" })
+        : res.status(401).json({ success: false, message: "Wrong OTP" })
     } else {
-      res.status(401).json({ message: "Somthing Went Wrong" });
+      res.status(401).json({ success: false, message: "Somthing Went Wrong" })
     }
-  });
-});
+  })
+})
 
 router.post("/signup", async (req, res) => {
   //console.log(req.body)
-  const { name, email, number, password, age, gender } = req.body;
+  const { name, email, number, password, age, gender } = req.body
   if (email) {
-    console.log(req.body);
-    let phoneNumber = "";
+    console.log(req.body)
+    let phoneNumber = ""
     User.findOne({ email: email }).then(async (user) => {
-      console.log(user);
+      console.log(user)
       if (user) {
         res.status(401).json({
           message: "User Already Exists",
-        });
+        })
       } else {
         try {
           const user = new User({
@@ -171,25 +173,25 @@ router.post("/signup", async (req, res) => {
             password,
             age,
             gender,
-          });
-          console.log(user);
-          await user.save();
+          })
+          console.log(user)
+          await user.save()
 
-          const token = jwt.sign({ userId: user._id }, "My_Secret_Key");
-          res.send({ token, user });
+          const token = jwt.sign({ userId: user._id }, "My_Secret_Key")
+          res.send({ token, user })
         } catch (err) {
-          return res.status(401).send(err.message);
+          return res.status(401).send(err.message)
         }
       }
-    });
+    })
   } else {
-    console.log(req.body);
-    let email = "";
+    console.log(req.body)
+    let email = ""
     User.findOne({ phoneNumber: number }).then(async (user) => {
       if (user) {
         res.status(401).json({
           message: "User Already Exists",
-        });
+        })
       } else {
         try {
           const user = new User({
@@ -198,58 +200,58 @@ router.post("/signup", async (req, res) => {
             password,
             age,
             gender,
-          });
-          await user.save();
+          })
+          await user.save()
           //console.log(user);
 
-          const token = jwt.sign({ userId: user._id }, "My_Secret_Key");
-          res.send({ token, user });
+          const token = jwt.sign({ userId: user._id }, "My_Secret_Key")
+          res.send({ token, user })
         } catch (err) {
-          return res.status(401).send(err.message);
+          return res.status(401).send(err.message)
         }
       }
-    });
+    })
   }
-});
+})
 
 router.post("/signin", async (req, res) => {
-  const { email, number, password } = req.body;
+  const { email, number, password } = req.body
 
   if (email) {
     if (!email || !password) {
       return res
         .status(401)
-        .send({ error: "you must provide email/number and password :)" });
+        .send({ error: "you must provide email/number and password :)" })
     }
-    const user = await User.findOne({ email });
+    const user = await User.findOne({ email })
     if (!user) {
-      return res.status(401).send({ error: "invalid password or email :)" });
+      return res.status(401).send({ error: "invalid password or email :)" })
     }
     try {
-      await user.comparePassword(password);
-      const token = jwt.sign({ userId: user._id }, "My_Secret_Key");
-      res.send({ token });
+      await user.comparePassword(password)
+      const token = jwt.sign({ userId: user._id }, "My_Secret_Key")
+      res.send({ token })
     } catch (err) {
-      return res.status(401).send({ error: "invalid password or email :)" });
+      return res.status(401).send({ error: "invalid password or email :)" })
     }
   } else {
     if (!number || !password) {
       return res
         .status(401)
-        .send({ error: "you must provide email/number and password :)" });
+        .send({ error: "you must provide email/number and password :)" })
     }
-    const user = await User.findOne({ phoneNumber: number });
+    const user = await User.findOne({ phoneNumber: number })
     if (!user) {
-      return res.status(401).send({ error: "invalid password or email :)" });
+      return res.status(401).send({ error: "invalid password or email :)" })
     }
     try {
-      await user.comparePassword(password);
-      const token = jwt.sign({ userId: user._id }, "My_Secret_Key");
-      res.send({ token });
+      await user.comparePassword(password)
+      const token = jwt.sign({ userId: user._id }, "My_Secret_Key")
+      res.send({ token })
     } catch (err) {
-      return res.status(401).send({ error: "invalid password or email :)" });
+      return res.status(401).send({ error: "invalid password or email :)" })
     }
   }
-});
+})
 
-module.exports = router;
+module.exports = router
